309590058db17bdf8a895e2fb29e616570a58c27
BaneleMlamleli <mlamlelibanele@yahoo.com>
2024-12-02 00:14:32 +0200

    Implemented methods to add in item in the cart and click on the cart to verify the amounts of items and items selected

--- src/test/java/com/saucedemo/tests/CartTest.java
+++ src/test/java/com/saucedemo/tests/CartTest.java
@@ -1,7 +1,19 @@
 package com.saucedemo.tests;
 
+import org.testng.annotations.Test;
+
 import com.saucedemo.core.BaseClass;
+import com.saucedemo.pages.Cart;
 
 public class CartTest extends BaseClass {
 
+    @Test(priority = 1)
+    public void validateSpanIsDisplayed() {
+        new Cart(driver).validateSpanItemIsDisplayed();
+    }
+
+    @Test(priority = 2)
+    public void validateCartWithItems() {
+        new Cart(driver).clickCartWithItems();
+    }
 }
